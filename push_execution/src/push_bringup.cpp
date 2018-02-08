df200d24ef51813e19da7d1dddca5f28ab6f1e79
Henning Kayser <1kayser@informatik.uni-hamburg.de>
2018-02-08 10:41:36 +0100

    Implementation of simple push operation prototypes
    PushBringup class offers a simple command line interface for maintenance
    and demo push movements.
    Pusher class implements push trajectory computation from push messages.

--- push_execution/src/push_bringup.cpp
+++ push_execution/src/push_bringup.cpp
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <stdio.h>
+//#include <ncurses.h>
 
 #include <ros/ros.h>
 
@@ -7,55 +9,154 @@
 #include <geometry_msgs/Quaternion.h>
 #include <geometry_msgs/Pose.h>
 
+std::string COMMAND_MAINTENANCE = "m";
+std::string COMMAND_GRIPPER_OPEN = "o";
+std::string COMMAND_GRIPPER_CLOSE = "c";
+std::string COMMAND_DEMO = "r";
+std::string COMMAND_HELP = "h";
+std::string COMMAND_QUIT = "q";
+
+
+class PushBringup
+{
+	public:
+	geometry_msgs::Quaternion orientation_down_ = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.5*M_PI, 0.0);
+	geometry_msgs::Quaternion orientation_up_ = tf::createQuaternionMsgFromRollPitchYaw(0.0, -0.5*M_PI, 0.0);
+	moveit::planning_interface::MoveGroupInterface arm_;
+	moveit::planning_interface::MoveGroupInterface gripper_;
+
+
+		PushBringup() : arm_("arm"), gripper_("gripper"){
+		};
+
+		bool moveToMaintenancePose() {
+			// move to initial pose
+			arm_.setNamedTarget("push_maintenance");
+			return bool(arm_.move());
+		}
+
+		bool moveToUpPose(double z_val) {
+			// move gripper up to mount pusher
+			geometry_msgs::Pose pose;
+			pose.position.z = z_val;
+			pose.orientation = orientation_up_;
+			arm_.setPoseReferenceFrame("table_top");
+			arm_.setPoseTarget(pose);
+			return bool(arm_.move());
+		}
+
+		bool moveToDownPose(double z_val) {
+			// move gripper up to mount pusher
+			geometry_msgs::Pose pose;
+			pose.position.z = z_val;
+			pose.orientation = orientation_down_;
+			arm_.setPoseReferenceFrame("table_top");
+			arm_.setPoseTarget(pose);
+			return bool(arm_.move());
+		}
+
+		bool openGripper() {
+			gripper_.setNamedTarget("open");
+			gripper_.move();
+		}
+
+		bool closeGripper() {
+			gripper_.setNamedTarget("closed");
+			gripper_.move();
+		}
+
+		void wait(float seconds) {
+			ros::Duration(seconds).sleep();
+		}
+
+		bool runDemoMovement(int times=0) {
+			// compute and perform 8 movement
+			moveToDownPose(0.04);
+			geometry_msgs::Pose pose = arm_.getCurrentPose().pose;
+			arm_.setPoseReferenceFrame("table_top");
+			//geometry_msgs::Pose pose;
+			pose.position.z = 0.04;
+			//pose.orientation = orientation_down_;
+			moveit::planning_interface::MoveGroupInterface::Plan plan;
+			std::vector<geometry_msgs::Pose> waypoints;
+			float radius = 0.05;
+			double angle = 0.0;
+			for(int i = 0; i < 50; i++) {
+				angle = 2 * M_PI * i / 50.0;
+				pose.position.x = sin(2 * angle) * radius;
+				pose.position.y = -sin(angle) * radius;
+				waypoints.push_back(pose);
+			}
+			float success = arm_.computeCartesianPath(waypoints, 0.03, 3, plan.trajectory_);
+			if(success==1.0) {
+				int count = 0;
+				// TODO: add keyboard callback
+				while((times==0 || count++ <= times)) {
+					if (!arm_.execute(plan)) 
+						return false;
+				}
+				return true;
+			} else {
+				ROS_INFO_STREAM("\nFailed 8 trajectory with percentage " << success);
+				return false;
+			}
+		}
+};
+
+void printHelp() {
+	std::cout << std::endl;
+	std::cout << "========================================" << std::endl;
+	std::cout << COMMAND_MAINTENANCE << " - move to maintenance pose" << std::endl;
+	std::cout << COMMAND_GRIPPER_OPEN << " - open gripper" << std::endl;
+	std::cout << COMMAND_GRIPPER_CLOSE << " - close gripper" << std::endl;
+	std::cout << COMMAND_DEMO << " - run demo" << std::endl;
+	std::cout << COMMAND_HELP << " - show help screen" << std::endl;
+	std::cout << COMMAND_QUIT << " - quit program" << std::endl;
+}
+
 
 int main(int argc, char** argv) {
 	geometry_msgs::Quaternion down = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.5*M_PI, 0.0);
 	geometry_msgs::Quaternion up = tf::createQuaternionMsgFromRollPitchYaw(0.0, -0.5*M_PI, 0.0);
-	ros::init(argc,argv, "push_bringup_node");
+	ros::init(argc,argv, "push_bringup_node", ros::init_options::NoRosout);
 	ros::AsyncSpinner spinner(4);
 	spinner.start();
-	
-	//initialize movegroup
-	moveit::planning_interface::MoveGroupInterface arm("arm");
+	PushBringup pb;
 
-	// move to initial pose
-	arm.setNamedTarget("pour_default");
-	arm.move();
+	std::cout << "========================================" << std::endl;
+	std::cout << std::endl << "Launching Push operator bringup" << std::endl;
 
-	// move gripper up to mount pusher
-	geometry_msgs::Pose pose;
-	pose.position.z = 0.5;
-	pose.orientation = up;
-	arm.setPoseReferenceFrame("table_top");
-	arm.setPoseTarget(pose);
-	arm.move();
+	printHelp();
+	std::cout << std::endl << "Waiting for input:";
 
-	ros::Duration(3).sleep();
+	std::string input;
 
-	// move pusher down to table
-	pose.position.z = 0.04;
-	pose.orientation = down;
-	arm.setPoseTarget(pose);
-	arm.move();
+	while(ros::ok()) {
+		std::cout << std::endl << ">";
 
-	// compute and perform 8 movement
-	moveit::planning_interface::MoveGroupInterface::Plan plan;
-	std::vector<geometry_msgs::Pose> waypoints;
-	float radius = 0.05;
-	double angle = 0.0;
-	for(int i = 0; i < 50; i++) {
-		angle = 2 * M_PI * i / 50.0;
-		pose.position.x = sin(2 * angle) * radius;
-		pose.position.y = -sin(angle) * radius;
-		waypoints.push_back(pose);
-	}
-	float success = arm.computeCartesianPath(waypoints, 0.03, 3, plan.trajectory_);
-	if(success==1.0) {
-		while(ros::ok()) {
-			arm.execute(plan);
+		// Display prompt and read input (NB: input must be freed after use)...
+		std::getline(std::cin, input);
+		if(input == COMMAND_MAINTENANCE) {
+			std::cout << "Moving to maintenance pose" << std::endl;
+			pb.moveToMaintenancePose();
+		} else if (input == COMMAND_GRIPPER_OPEN){
+			std::cout << "Opening gripper" << std::endl;
+			pb.openGripper();
+		} else if (input == COMMAND_GRIPPER_CLOSE){
+			std::cout << "Closing gripper" << std::endl;
+			pb.closeGripper();
+		} else if (input == COMMAND_DEMO){
+			std::cout << "Running demo push movement." << std::endl;
+			pb.runDemoMovement(4);
+		} else if (input == COMMAND_HELP){
+			printHelp();
+		} else if (input == COMMAND_QUIT){
+			std::cout << "Bye!" << std::endl;
+			break;
+		} else {
+			std::cout << "Unkown command: '" << input  << "'" << std::endl;
+			printHelp();
 		}
-	} else {
-		ROS_INFO_STREAM("Failed 8 trajectory with percentage " << success);
 	}
 	return 0;
 }
--- push_execution/src/pusher.cpp
+++ push_execution/src/pusher.cpp
@@ -1,9 +1,16 @@
 #include <ros/ros.h>
 
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit_msgs/CollisionObject.h>
 
-namespace push_execution
+#include <tams_ur5_push_execution/Push.h>
+#include <tams_ur5_push_execution/PushApproach.h>
+
+std::random_device rd;
+std::mt19937 gen{rd()};
+
+namespace tams_ur5_push_execution
 {
 	class Pusher
 	{
@@ -11,60 +18,187 @@ namespace push_execution
 
 			moveit_msgs::CollisionObject& obj_;
 
-			std::string& frame_id_;
+			moveit::planning_interface::MoveGroupInterface& group_;
 
-			const moveit::planning_interface::MoveGroupInterface& group_;
+			Pusher(moveit::planning_interface::MoveGroupInterface& group,
+					moveit_msgs::CollisionObject& obj) : obj_(obj), group_(group){};
 
-
-			Pusher(const moveit::planning_interface::MoveGroupInterface& group,
-					moveit_msgs::CollisionObject& obj,
-					std::string& frame_id) : obj_(obj), frame_id_(frame_id), group_(group){};
-
-			void setTargetObject(moveit_msgs::CollisionObject& obj)
-			{
+			void setTargetObject(moveit_msgs::CollisionObject& obj) {
 				obj_ = obj;
 			}
 
-			void setPoseReferenceFrame(std::string& frame_id)
-			{
-				frame_id_ = frame_id;
-			}
+			void performRandomPush() {
+				Push push;
+				push.mode = Push::LINEAR;
+				push.approach = sampleRandomPushApproach(obj_);
+				push.distance = 0.05;
+				moveit::planning_interface::MoveGroupInterface::Plan plan;
+				robot_state::RobotState rstate(*group_.getCurrentState());
+				if(computeCartesianPushTraj(push, plan.trajectory_, rstate)) {
+					// move to pre-push position
+					group_.setJointValueTarget(rstate);
+					group_.move();
 
-			bool executeRandomPush()
-			{
-				geometry_msgs::Pose push_pose = sampleRandomPushPose(obj_);
+					// push object
+					group_.execute(plan);
+
+					// move endeffector up
+					geometry_msgs::PoseStamped pose = group_.getCurrentPose();
+					pose.pose.position.z += 0.2;
+					group_.setJointValueTarget(pose);
+					group_.move();
+				} else {
+					ROS_INFO_STREAM("Failed to plan and execute push trajectory!");
+				}
 			}
 
 		private:
 
-			geometry_msgs::Pose sampleRandomPushPose(moveit_msgs::CollisionObject& obj)
-			{
-				geometry_msgs::Pose push_pose;
-				push_pose.position = sampleRandomContactPoint(obj);
+			PushApproach sampleRandomPushApproach(moveit_msgs::CollisionObject& obj) {
+				PushApproach approach;
+				approach.frame_id = obj.header.frame_id;
 
-				push_pose.orientation = sampleRandomPushAngle(obj, push_pose.position);
+				geometry_msgs::Pose approach_pose;
+				sampleRandomContactPoint(obj, approach_pose);
 
-				return push_pose;
+				approach.point = approach_pose.position;
+				approach.normal = approach_pose.orientation;
+				approach.angle = sampleRandomPushAngle();
+				return approach;
 			}
 
-			geometry_msgs::Point sampleRandomContactPoint(moveit_msgs::CollisionObject& obj)
-			{
+			bool computeCartesianPushTraj(tams_ur5_push_execution::Push& push, moveit_msgs::RobotTrajectory& trajectory, robot_state::RobotState& rstate) {
+				if(push.mode == tams_ur5_push_execution::Push::LINEAR) {
+					// extract push direction and point
+					tf::Quaternion push_orientation;
+					tf::quaternionMsgToTF(tf::createQuaternionMsgFromYaw(push.approach.angle), push_orientation);
+					tf::Quaternion push_normal;
+					tf::quaternionMsgToTF(push.approach.normal, push_normal);
+					tf::Quaternion push_direction = push_normal + push_orientation;
+					tf::Vector3 approach_point;
+					tf::pointMsgToTF(push.approach.point, approach_point);
+
+					tf::Transform approach;
+					approach.setOrigin(approach_point);
+					approach.setRotation(push_direction);
+
+					tf::Transform start_point;
+					geometry_msgs::Point start;
+					start.x = -0.1; //TODO: move pre-approach distance to msg
+					tf::Vector3 start_v;
+					tf::pointMsgToTF(start,start_v);
+					start_point.setOrigin(start_v);
+					start_point.setRotation(tf::Quaternion::getIdentity());
+					start_point *= approach;
+
+					tf::Transform goal_point;
+					geometry_msgs::Point goal;
+					start.x = push.distance;
+					tf::Vector3 goal_v;
+					tf::pointMsgToTF(goal,goal_v);
+					goal_point.setOrigin(goal_v);
+					goal_point.setRotation(tf::Quaternion::getIdentity());
+					goal_point *= approach;
+
+					std::vector<geometry_msgs::Pose> waypoints;
+					int step_count = 10;
+					tf::Vector3 step = (goal_point.getOrigin() - start_point.getOrigin()) / step_count;
+					tf::Vector3 start_origin = start_point.getOrigin();
+					geometry_msgs::Pose wp;
+					wp.orientation = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.5*M_PI, 0.0);
+
+					for(int i = 0; i <= step_count; i++) {
+						start_point.setOrigin(start_origin + i * step);
+						tf::poseTFToMsg(start_point, wp);
+						waypoints.push_back(wp);
+					}
+
+					group_.setPoseReferenceFrame(push.approach.frame_id);
+					group_.setJointValueTarget(waypoints[0]);
+					rstate = group_.getJointValueTarget();
+					group_.setStartState(rstate);
+					float success_fraction = group_.computeCartesianPath(waypoints, 0.03, 3, trajectory);
+					group_.setStartStateToCurrentState();
+					group_.clearPoseTargets();
+					if(success_fraction == 1.0) {
+						return true;
+					}
+				}
+				return false;
+			}
+
+			bool sampleRandomContactPoint(moveit_msgs::CollisionObject& obj, geometry_msgs::Pose& pose) {
+				// we expect a single BOX primitive for now
 				geometry_msgs::Point contact_point;
 				//Sample contact point from obj shape
-				return contact_point;
+				if(obj.primitives[0].type == shape_msgs::SolidPrimitive::BOX) {
+					double dim_x = obj.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_X];
+					double dim_y = obj.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Y];
+					pose.position.z = obj.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Z];
+					double box_len = 2 * (dim_x + dim_y);
+					std::uniform_real_distribution<> dis(0.0, box_len);
+					double p = dis(gen);
+					if(p < dim_x) {
+						pose.position.x = p;
+						pose.position.y = 0;
+						pose.orientation.w = 1;
+					} else if (p < dim_x + dim_y) {
+						pose.position.x = dim_x;
+						pose.position.y = p - dim_y;
+						pose.orientation = tf::createQuaternionMsgFromYaw(0.5*M_PI);
+					} else if (p < 2 * dim_x + dim_y) {
+						pose.position.x = 2 * dim_x + dim_y - p;
+						pose.position.y = dim_y;
+						pose.orientation = tf::createQuaternionMsgFromYaw(M_PI);
+					} else {
+						pose.position.x = 0;
+						pose.position.y = 2 * (dim_x + dim_y) - p;
+						pose.orientation = tf::createQuaternionMsgFromYaw(1.5*M_PI);
+					}
+					// adjust to center
+					pose.position.x -= 0.5*dim_x;
+					pose.position.y -= 0.5*dim_y;
+					return true;
+				}
+				return false;
 			}
 
-			geometry_msgs::Quaternion sampleRandomPushAngle(moveit_msgs::CollisionObject& obj, geometry_msgs::Point contact_point)
-			{
-				geometry_msgs::Quaternion push_angle;
-				//Sample push angle from contact point
-				return push_angle;
-			}
-
-			bool executePush(geometry_msgs::Pose push_pose, double push_dist=0.02, double approach_dist=0.05, double retrace_dist=0.03)
-			{
-				//compute cartesian path from pre push, push and retrace
-				return true;
+			float sampleRandomPushAngle() {
+				std::normal_distribution<> d{0,0.2};
+				return d(gen);
 			}
 	};
 }
+
+moveit_msgs::CollisionObject spawnObject(moveit::planning_interface::PlanningSceneInterface& psi, const std::string& obj_id, double x_dim, double y_dim, double z_dim, double x_pos=0.0, double y_pos=0.0){
+	moveit_msgs::CollisionObject obj;
+	obj.id = obj_id;
+	obj.header.frame_id = "table_top";
+	obj.primitive_poses.resize(1);
+	obj.primitive_poses[0].position.x = x_pos;
+	obj.primitive_poses[0].position.y = y_pos;
+	obj.primitive_poses[0].position.z = 0.5 * z_dim;
+	obj.primitive_poses[0].orientation.w = 1;
+	obj.primitives.resize(1);
+	obj.primitives[0].type = shape_msgs::SolidPrimitive::BOX;
+	obj.primitives[0].dimensions.resize(3);
+	obj.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_X] = x_dim;
+	obj.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Y] = y_dim;
+	obj.primitives[0].dimensions[shape_msgs::SolidPrimitive::BOX_Z] = z_dim;
+	//psi.applyCollisionObject(obj);
+	return obj;
+}
+
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "push_execution_node");
+	ros::AsyncSpinner spinner(4);
+	spinner.start();
+	moveit::planning_interface::PlanningSceneInterface psi;
+	moveit_msgs::CollisionObject box = spawnObject(psi, "box", 0.05, 0.1, 0.05); 
+	moveit::planning_interface::MoveGroupInterface arm("arm");
+	tams_ur5_push_execution::Pusher pusher(arm, box);
+	pusher.performRandomPush();
+	return 0;
+}
+
+
