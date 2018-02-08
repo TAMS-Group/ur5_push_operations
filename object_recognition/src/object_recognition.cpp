300a8754aff152ba39344190f24051181d88f811
Henning Kayser <1kayser@informatik.uni-hamburg.de>
2018-02-08 15:02:37 +0100

    Use object configurations for marker visualization
    The object recognition now loads known object description from
    objects.yaml inside ur5_push_setup.
    In demo mode a marker with the specified shape is spawned at <0,0,0>
    (table_top) under a dedicated child frame.
    When using the hardware the recongized april tags should be mapped to
    the object and the markers frame is adjusted online.

--- object_recognition/src/object_recognition.cpp
+++ object_recognition/src/object_recognition.cpp
@@ -1,73 +1,179 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
+#include <visualization_msgs/Marker.h>
 
 #include <apriltags_ros/AprilTagDetection.h>
 #include <apriltags_ros/AprilTagDetectionArray.h>
 
+#include <XmlRpcValue.h>
+#include <XmlRpcException.h>
+
+std::string MARKER_TOPIC = "pushable_objects";
+std::string MARKER_NAMESPACE = "pushable_objects";
+
 class ObjectRecognitionNode {
     private:
+        ros::NodeHandle nh_;
+        ros::NodeHandle ph_;
+
         ros::Subscriber sub_;
 
         // TF communication channels
         tf::TransformListener tf_listener_;
         tf::TransformBroadcaster tf_broadcaster_;
 
+        // Marker Publisher
+        ros::Publisher marker_pub_;
+
         // measured object
         tf::Transform object_transform_;
         int object_tag_id_;
-        std::string object_frame_id_;
+        std::string object_frame_prefix_;
         ros::Time detection_time_;
 
         //ros::Time timeout_;
-
         bool initialized_;
-        bool dummy_mode_;
+        bool demo_mode_;
+        int demo_object_id_;
+
+        XmlRpc::XmlRpcValue objects_;
 
     public:
         ObjectRecognitionNode() : initialized_(false) {
-            ros::NodeHandle node;
-            ros::NodeHandle private_handle("~");
-            private_handle.param<bool>("dummy_mode", dummy_mode_, false);
-            private_handle.param<int>("object_tag_id", object_tag_id_, 11);
-            private_handle.param<std::string>("object_frame_id", object_frame_id_, "/object_frame_" + std::to_string(object_tag_id_));
-            //private_handle.param<double>("timeout", timeout_, 10);
-            if (!dummy_mode_) {
-                sub_ = node.subscribe("tag_detections", 1, &ObjectRecognitionNode::callback, this);
-            } else {
-                geometry_msgs::Pose pose;
-                pose.orientation.w = 1;
-                pose.position.z = 0.05;
-                ros::Rate rate(20);
-                while(ros::ok()) {
-                    tf::poseMsgToTF(pose, object_transform_);
-                    tf_broadcaster_.sendTransform(tf::StampedTransform(object_transform_, ros::Time::now(), "/table_top", object_frame_id_));
-                    rate.sleep();
+            if(!initialized_) {
+                ROS_INFO_STREAM("ObjectRecognitionNode running!");
+                initialized_ = true;
+                nh_ = (*new ros::NodeHandle());
+                ph_ = (*new ros::NodeHandle("~"));
+
+                // marker publisher
+                marker_pub_ = nh_.advertise<visualization_msgs::Marker>(MARKER_TOPIC, 0);
+
+                // load parameters
+                loadParams();
+
+                if (demo_mode_) {
+                    // create marker and transform
+                    visualization_msgs::Marker marker;
+                    if(createObjectMarker(0, marker)) {
+                        geometry_msgs::Pose pose;
+                        tf::StampedTransform transform = createNormalizedTransform(pose, marker);
+                        ros::Rate rate(20);
+                        while(ros::ok()) {
+                            publishTransformAndMarker(transform, marker);
+                            rate.sleep();
+                        }
+                    } else {
+                        ROS_WARN_STREAM("Failed to construct Marker from object configuration!");
+                    }
+                } else {
+                    sub_ = nh_.subscribe("tag_detections", 1, &ObjectRecognitionNode::onDetectAprilTags, this);
                 }
             }
         }
 
-        void callback(const apriltags_ros::AprilTagDetectionArray& msg){
+        void loadParams(){
+            // object descriptions and topic
+            ph_.getParam("objects", objects_);
+            ph_.param<std::string>("object_frame_prefix_", object_frame_prefix_, "/pushable_object_");
+
+            // demo parameters
+            ph_.param<bool>("demo_mode", demo_mode_, false);
+            ph_.param<int>("demo_object_id_", demo_object_id_, 0);
+
+            // april tag information
+            // TODO: specify object tag mapping
+            ph_.param<int>("object_tag_id", object_tag_id_, 11);
+            //ph_.param<double>("timeout", timeout_, 10);
+        }
+
+        void publishTransformAndMarker(tf::StampedTransform& transform, visualization_msgs::Marker& marker) {
+            transform.stamp_ = ros::Time::now();
+            tf_broadcaster_.sendTransform(transform);
+            marker_pub_.publish(marker);
+        }
+
+        void onDetectAprilTags(const apriltags_ros::AprilTagDetectionArray& msg){
             for(apriltags_ros::AprilTagDetection detection : msg.detections) {
                 if(detection.id == object_tag_id_) {
-                    tf::poseMsgToTF(detection.pose.pose, object_transform_);
-                    detection_time_ = detection.pose.header.stamp;
-                    if(!initialized_) {
-                        ROS_INFO("object recognition is running");
-                        initialized_ = true;
+                    // create marker
+                    visualization_msgs::Marker marker;
+                    if(createObjectMarker(object_tag_id_, marker)) {
+                        // extract transform from pose
+                        detection_time_ = detection.pose.header.stamp;
+                        geometry_msgs::PoseStamped pose = detection.pose;
+                        tf_listener_.transformPose("/table_top", pose, pose);
+                        tf::StampedTransform transform = createNormalizedTransform(detection.pose.pose, marker);
+                        publishTransformAndMarker(transform, marker);
+                        break;
+                    } else {
+                        ROS_WARN_STREAM("Failed to create marker for object " << object_tag_id_ << "!");
                     }
-
-                    //Send transform with actually measured detection time
-                    tf_broadcaster_.sendTransform(tf::StampedTransform(object_transform_, detection_time_, "/world", object_frame_id_));
-                    break;
                 }
             }
+        }
+
+        bool createObjectMarker(int object_id, visualization_msgs::Marker& marker) {
+            marker.header.frame_id = object_frame_prefix_ + std::to_string(object_id);
+            marker.header.stamp = ros::Time();
+            marker.ns = MARKER_NAMESPACE;
+            marker.id = object_id;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.orientation.w = 1;
+            marker.color.a = 0.9;
+            marker.color.r = 0.0;
+            marker.color.g = 1.0;
+            marker.color.b = 0.0;
+
+            try {
+                XmlRpc::XmlRpcValue& val = objects_[object_id].begin()->second;
+                std::string marker_type = val["type"];
+                if(marker_type == "CUBE") {
+                    marker.type = visualization_msgs::Marker::CUBE;
+                } else if (marker_type == "SPHERE") {
+                    marker.type = visualization_msgs::Marker::SPHERE;
+                } else if (marker_type == "CYLINDER") {
+                    marker.type = visualization_msgs::Marker::CYLINDER;
+                } else if (marker_type == "MESH") {
+                    marker.type = visualization_msgs::Marker::MESH_RESOURCE;
+                    if(val["mesh"].valid()) {
+                        marker.mesh_resource = static_cast<std::string>(val["mesh"]);
+                    } else {
+                        ROS_WARN_STREAM("Incomplete definition of pushable object " << object_id << ".");
+                        ROS_WARN_STREAM("Object type is MESH but 'mesh_resource' is not specified!");
+                        return false;
+                    }
+                } else {
+                    ROS_WARN_STREAM("Found unknown marker type '" << marker_type << "' in definition of object " << object_id);
+                    return false;
+                }
+                marker.scale.x = val["scale"]["x"];
+                marker.scale.y = val["scale"]["y"];
+                marker.scale.z = val["scale"]["z"];
+            } catch (XmlRpc::XmlRpcException& e) {
+                ROS_WARN_STREAM("Error extracting values of object " << object_id << " from configuration file!");
+                ROS_WARN("%s", e.getMessage().c_str());
+                return false;
+            }
+            return true;
+        }
+
+        tf::StampedTransform createNormalizedTransform(geometry_msgs::Pose& pose, visualization_msgs::Marker& marker) {
+            //TODO: correct rotation to align with the table!
+            pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
+            pose.position.z = 0.5 * marker.scale.z;
+            tf::Transform transform;
+            tf::poseMsgToTF(pose, transform);
+            return tf::StampedTransform(transform, detection_time_, "/table_top", object_frame_prefix_ + std::to_string(marker.id));
         }
 };
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "object_recognition_node");
+
     ObjectRecognitionNode obj_rec;
+    ROS_INFO_STREAM("ObjectRecognitionNode running!");
     ros::spin();
     return 0;
 };
