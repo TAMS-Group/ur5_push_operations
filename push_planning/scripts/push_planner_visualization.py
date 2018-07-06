23e305f69bd0d332bb17df6ec50e18e89c39a9de
Henning Kayser <henningkayser@mail.de>
2018-07-06 15:04:07 +0200

    use get_marker() for box marker initialization

--- push_planning/scripts/push_planner_visualization.py
+++ push_planning/scripts/push_planner_visualization.py
@@ -11,7 +11,7 @@ dim_X = 0.162
 dim_Y = 0.23
 dim_Z = 0.112
 
-def visualize_push_trajectory(traj_msg):
+def visualize_object_trajectory(traj_msg):
     global marker_pub
     markers = MarkerArray()
 
@@ -22,46 +22,16 @@ def visualize_push_trajectory(traj_msg):
     color = ColorRGBA(0.0,1.0,0.0,1.0)
     visualize_graph(nodes, edges, nodes_id=2, edges_id=3, nodes_color=color, edges_color=color, linewidth=0.003, is_path=True)
 
-    # show box path
+    # visualize object path with markers
+    # TODO: we might use the existing marker to copy the actual shape instead of using hard coded values
     for i,pose in enumerate(traj_msg.poses):
-        marker = Marker()
-        marker.header.frame_id = "/table_top"
-        marker.type = marker.CUBE
-        marker.action = marker.ADD
-        marker.scale.x = dim_X
-        marker.scale.y = dim_Y
-        marker.scale.z = dim_Z
-        marker.color.a = 0.1
         progress = float(i) / len(traj_msg.poses)
-        marker.color.b = 1 - progress
-        marker.color.r = progress
-        marker.id = i
-        marker.pose = pose
-        marker.pose.position.z = 0.5*dim_Z
-        markers.markers.append(marker)
+        color = ColorRGBA(progress,0.0,1-progress,0.1)
+        scale = Vector3(dim_X, dim_Y, dim_Z)
+        pose.position.z = 0.5*dim_Z
+        markers.markers.append(getMarker(i, Marker.CUBE, scale=scale, pose=pose, color=color))
+
     marker_pub.publish(markers)
-
-
-
-#def visualize_planner_data(graph_msg):
-#    global data_pub
-#
-#    pose = Pose()
-#    pose.position.z = 0.5 * dim_Z
-#    pose.orientation.w = 1.0
-#
-#    points = getMarker(0, Marker.POINTS, scale=Vector3(0.0025, 0.0025, 0.0), pose=pose)
-#    lines = getMarker(1, Marker.LINE_LIST, scale=Vector3(0.001,0,0), pose=pose)
-#
-#    for i,(point, adjacent) in enumerate(zip(graph_msg.nodes, graph_msg.edges)):
-#        points.points.append(point)
-#        points.colors.append(ColorRGBA(1.0,0.0,0.0,1.0))
-#        for n in adjacent.node_ids:
-#            lines.points.append(point)
-#            lines.points.append(graph_msg.nodes[n])
-#        
-#    data_pub.publish(points)
-#    data_pub.publish(lines)
 
 def visualize_planner_data(graph_msg):
     visualize_graph(graph_msg.nodes, graph_msg.edges)
@@ -100,33 +70,6 @@ def visualize_graph(nodes, edges, nodes_id=0, edges_id=1,
     data_pub.publish(points)
     data_pub.publish(lines)
 
-
-
-# attempt to visualize weights - however all weights are set to 1.0
-#def visualize_planner_data(graph_msg):
-#    global data_pub
-#
-#    pose = Pose()
-#    pose.position.z = 0.5 * dim_Z
-#    pose.orientation.w = 1.0
-#
-#    id_count = 0
-#    points = getMarker(id_count, Marker.POINTS, scale=Vector3(0.0025, 0.0025, 0.0), pose=pose)
-#
-#    for i,(point, adjacent) in enumerate(zip(graph_msg.nodes, graph_msg.edges)):
-#        points.points.append(point)
-#        points.colors.append(ColorRGBA(1.0,0.0,0.0,1.0))
-#        for n, weight in zip(adjacent.node_ids, adjacent.weights):
-#            id_count += 1
-#            if weight < 1.0:
-#                print weight
-#            lines = getMarker(id_count, Marker.LINE_LIST, scale=Vector3(0.001 * weight,0,0), pose=pose)
-#            lines.points.append(point)
-#            lines.points.append(graph_msg.nodes[n])
-#            data_pub.publish(lines)
-#        
-#    data_pub.publish(points)
-
 def getMarker(marker_id, marker_type, pose=None, scale=None, color=None):
     marker = Marker()
     marker.header.frame_id = "/table_top"
@@ -148,7 +91,7 @@ def visualization_node():
     global marker_pub, data_pub
     rospy.init_node('push_trajectory_visualization_node');
 
-    traj_sub = rospy.Subscriber('/push_trajectory', PushTrajectory, visualize_push_trajectory, queue_size=1)
+    traj_sub = rospy.Subscriber('/push_trajectory', PushTrajectory, visualize_object_trajectory, queue_size=1)
     data_sub = rospy.Subscriber('/push_planner_graph', GeometryGraph, visualize_planner_data, queue_size=1)
 
     marker_pub = rospy.Publisher("/push_trajectory_markers", MarkerArray, queue_size=1)
